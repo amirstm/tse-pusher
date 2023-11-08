8401dd2946d4a966dc9167be4edd91ff1abcdbc3
AmirSTM <amirstm@gmail.com>
2023-11-08 09:17:32 +0330

    Initiate __update_trade_data method

--- tsetmc_pusher/operation.py
+++ tsetmc_pusher/operation.py
@@ -24,6 +24,7 @@ class TsetmcRealtimeCrawler(TsetmcRealtimeCrawlerTiminigs):
 
     def __init__(self):
         self.market_realtime_date = MarketRealtimeData()
+        self.__trade_data_timeout = 500
 
     @classmethod
     async def sleep(cls, wakeup_at: time):
@@ -31,12 +32,23 @@ class TsetmcRealtimeCrawler(TsetmcRealtimeCrawlerTiminigs):
         timedelta = datetime.combine(datetime.today(), wakeup_at) - datetime.now()
         await asyncio.sleep(timedelta)
 
-    async def perform_market_time(self):
+    async def __update_trade_data(self):
+        """Updates trade data from TSETMC"""
+        self._LOGGER.info(
+            "Trade data catch started, timeout : %d", self.__trade_data_timeout
+        )
+
+    async def __perform_trade_data_loop(self):
         """Perform the tasks for the market open time"""
+        while datetime.now().time() < self.MARKET_END_TIME:
+            try:
+                await self.__update_trade_data()
+            except Exception as ex:
+                pass
 
     async def perform_daily(self):
         """Daily tasks for the crawler are called from here"""
         self._LOGGER.info("Daily tasks are starting.")
         await self.sleep(self.MARKET_START_TIME)
         self._LOGGER.info("Market is starting.")
-        await self.perform_market_time()
+        await self.__perform_trade_data_loop()
