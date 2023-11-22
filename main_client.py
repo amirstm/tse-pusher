2b3204cf782b8dae4ac6fdbc15fcbc3950149dbc
AmirSTM <amirstm@gmail.com>
2023-11-22 08:29:05 +0330

    Lint main client side executable

--- main_client.py
+++ main_client.py
@@ -1,10 +1,10 @@
+"""Sample executable python code for client side"""
+
 import os
-import sys
-from dotenv import load_dotenv
 import asyncio
 import logging
 from logging.handlers import TimedRotatingFileHandler
-from websockets.client import connect
+from dotenv import load_dotenv
 from tse_utils.models.instrument import Instrument, InstrumentIdentification
 from tsetmc_pusher.client import TsetmcClient
 
@@ -14,20 +14,8 @@ WEBSOCKET_HOST = os.getenv("WEBSOCKET_HOST")
 WEBSOCKET_PORT = os.getenv("WEBSOCKET_PORT")
 
 
-async def async_input(websocket, *args):
-    for arg in args:
-        await websocket.send(arg)
-    await asyncio.sleep(1)
-
-
-async def async_recv(websocket):
-    while True:
-        message = await websocket.recv()
-        print(f"Received: {message}")
-        print("___________________________________")
-
-
-async def main(*args):
+async def main():
+    """Main asynchronous executable method"""
     logger = logging.getLogger("tsetmc_pusher")
     formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s: %(message)s")
     logger.setLevel(logging.DEBUG)
@@ -63,4 +51,4 @@ async def main(*args):
 
 
 if __name__ == "__main__":
-    asyncio.run(main(sys.argv[1:]))
+    asyncio.run(main())
