ce7e1b6c20644957bb8176e58d75ef4361d4caed
AmirSTM <amirstm@gmail.com>
2023-11-27 10:39:26 +0330

    Update PyPi package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="tsetmc_pusher",
-    version="1.1.0",
+    version="1.1.1",
     author="Arka Equities & Securities",
     author_email="zare@arkaequities.com",
     description="Pusher for Tehran Stock Exchange data crawled from TSETMC website.",
